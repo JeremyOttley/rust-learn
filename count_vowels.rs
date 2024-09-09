9ca47c3726e61a8363b76e057b2b8a3739ae64d0
Jeremy Ottley <Gazaunga@gmail.com>
2024-09-09 09:06:29 -0400

    Update count_vowels.rs

--- count_vowels.rs
+++ count_vowels.rs
@@ -1,7 +1,19 @@
-fn is_vowel(c: char) -> bool {
-    match c {
-        'a' | 'e' | 'i' | 'o' | 'u' | 
-        'A' | 'E' | 'I' | 'O' | 'U' => true,
-        _ => false
+fn main() {
+    println!("{:?}", count_vowels::count("Jeremy"));
+}
+
+mod count_vowels {
+    fn is_vowel(c: &char) -> bool {
+        match c {
+            'a' | 'e' | 'i' | 'o' | 'u' | 
+            'A' | 'E' | 'I' | 'O' | 'U' => true,
+            _ => false
+        }
     }
+    
+    pub fn count(word: &str) -> u32 {
+        let vowels: Vec<char> = word.chars().filter(|c| is_vowel(c)).collect();
+        return vowels.len() as u32;
+    }
+
 }

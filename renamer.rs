640d6ae2062e20e6595e3f836580b717f451f7c5
Jeremy Ottley <Gazaunga@gmail.com>
2024-09-10 10:28:41 -0400

    Create renamer.rs

---
+++ renamer.rs
@@ -0,0 +1,18 @@
+fn is_image(s: &str) -> bool {
+        match s {
+            s if s.ends_with(".jpg") || 
+            s.ends_with(".png") || 
+            s.ends_with(".bmp") || 
+            s.ends_with(".jpeg") => true,
+            _ => false,
+        }
+}
+
+fn get_images() {
+    x
+}
+
+
+fn rename() {
+    x
+}

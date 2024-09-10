0332249772ae5ab868033be078e36118bdffecfc
Jeremy Ottley <Gazaunga@gmail.com>
2024-09-10 12:31:44 -0400

    Create for.rs

---
+++ for.rs
@@ -0,0 +1,13 @@
+let mut v = vec![1, 2, 3, 4, 5];
+
+for i in &v {
+    println!("A reference to {}", i);
+}
+
+for i in &mut v {
+    println!("A mutable reference to {}", i);
+}
+
+for i in v {
+    println!("Take ownership of the vector and its element {}", i);
+}

f51f940af74bc1fc920443f18346c381abc8a8b6
Jeremy Ottley <Gazaunga@gmail.com>
2024-09-11 19:26:52 -0400

    Update user_input.rs

--- user_input.rs
+++ user_input.rs
@@ -1,22 +1,24 @@
 use std::io;
+use std::io::Write;
 
 fn get_user_input() -> Option<String> {
-  
-    println!("Please enter a valid ISBN-13: ");
-  
+
+    print!("Please enter a valid ISBN-13: ");
+    std::io::stdout().flush().unwrap();
+
     let mut input = String::new();
-  
+
     io::stdin()
-      .read_line(&mut input)
-      .expect("Failed to read line");
-  
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
     if input.is_empty() {
         return None;
     }
-    return Some(input);
+    	return Some(input);
 }
 
 fn main() {
-    //use expect to unwrap an option like withDefault in Haskell or Elm
+	//use expect to unwrap an option like withDefault in Haskell or Elm
     println!("{:?}", get_user_input().expect("Failed to get input").trim());
 }

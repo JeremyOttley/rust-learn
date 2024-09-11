1072a86f7a59c503c115d8fdb4de2f9f9058507e
Jeremy Ottley <Gazaunga@gmail.com>
2024-09-11 11:57:49 -0400

    Update regex.rs

--- regex.rs
+++ regex.rs
@@ -1,2 +1,4 @@
+use regex::Regex; //crate
+
 // doi: 10.1215/9874738291023
-(?P<prefix>\d{2}\.\d{4})\/(?P<isbn>\d{12})(?P<check>\d |\w)
+let re = Regex::new(r"(?P<prefix>\d{2}\.\d{4})\/(?P<isbn>\d{12})(?P<check>\d |\w)").unwrap();

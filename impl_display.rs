a7b2b5a7a318edc221ad815f64a2a70f4391dfbb
Jeremy Ottley <Gazaunga@gmail.com>
2024-09-14 22:54:08 -0400

    Create impl_display.rs

---
+++ impl_display.rs
@@ -0,0 +1,19 @@
+use std::fmt;
+
+struct Matrix(f32, f32, f32, f32);
+
+impl fmt::Display for Matrix {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Write strictly the first element into the supplied output
+        // stream: `f`. Returns `fmt::Result` which indicates whether the
+        // operation succeeded or failed. Note that `write!` uses syntax which
+        // is very similar to `println!`.
+        write!(f, "{}, {}, {}, {}", self.0, self.1, self.2, self.3)
+    }
+}
+
+fn main() {
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{}", matrix);
+}

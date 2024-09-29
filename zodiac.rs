0bdbab6dffa62079614b22bbb78ac5322ae6e299
Jeremy Ottley <Gazaunga@gmail.com>
2024-09-29 12:19:14 -0400

    Create zodiac.rs

---
+++ zodiac.rs
@@ -0,0 +1,70 @@
+use std::error::Error;
+use std::fmt;
+
+fn main() -> Result<(), Box<dyn Error>> {
+
+    let f = String::from("You will get laid!");
+    let h = Horoscope(Sign::Scorpio, Fortune(f));
+    println!("{}", h);
+
+    Ok(())
+}
+
+#[derive(Debug)]
+struct Fortune(String);
+//struct Fortune(&'static str);
+
+impl fmt::Display for Fortune {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+//#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[derive(Debug)]
+pub enum Sign {
+  Aries,
+  Taurus,
+  Gemini,
+  Cancer,
+  Leo,
+  Virgo,
+  Libra,
+  Scorpio,
+  Sagittarius,
+  Capricorn,
+  Aquarius,
+  Pisces,
+}
+
+impl fmt::Display for Sign {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Sign::Aries => write!(f, "{}", "Aries"),
+            Sign::Taurus => write!(f, "{}", "Taurus"),
+            Sign::Gemini => write!(f, "{}", "Gemini"),
+            Sign::Cancer => write!(f, "{}", "Cancer"),
+            Sign::Leo => write!(f, "{}", "Leo"),
+            Sign::Virgo => write!(f, "{}", "Virgo"),
+            Sign::Libra => write!(f, "{}", "Libra"),
+            Sign::Scorpio => write!(f, "{}", "Scorpio"),
+            Sign::Sagittarius => write!(f, "{}", "Sagittarius"),
+            Sign::Capricorn => write!(f, "{}", "Capricorn"),
+            Sign::Aquarius => write!(f, "{}", "Aquarius"),
+            Sign::Pisces => write!(f, "{}", "Pisces"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Horoscope(Sign, Fortune);
+
+
+impl fmt::Display for Horoscope {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}: {}", self.0, self.1)
+    }
+}
+

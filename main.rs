b3102cf95c67b163a0027190b4d92163378f6133
Jeremy Ottley <Gazaunga@gmail.com>
2024-10-02 21:24:06 -0400

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,5 +1,7 @@
 use std::error::Error;
 
+// With Box<dyn Error>, you get runtime polymorphism, meaning the actual error type 
+// is only known at runtime, not statically
 fn main() -> Result<(), Box<dyn Error>> {
 
   // do stuff

8ce9e9cdda304787ae78024e6ab421a89684cb08
Jeremy Ottley <Gazaunga@gmail.com>
2024-10-05 10:07:33 -0400

    Update renamer.rs

--- renamer.rs
+++ renamer.rs
@@ -16,3 +16,30 @@ fn get_images() {
 fn rename() {
     x
 }
+//////////////////////////
+use rand::Rng;
+use rand::distributions::{Alphanumeric};
+
+fn generate_random_string(length: usize) -> String {
+    let rng = rand::thread_rng();
+    rng.sample_iter(&Alphanumeric)
+        .take(length)
+        .map(|byte| byte as char)
+        .collect::<String>()
+}
+
+fn frobnicate(files: [&'static str]) -> [&'static str] {
+    let mut my_vec: Vec<&str> = Vec::new();
+    for file in files {
+        let new_name = format!("{}.{}", generate_random_string(10), file.split(".").last().unwrap());
+        my_vec.push(&new_name)
+    }
+}
+
+
+// Example usage
+fn main() {
+    let files = &["file.png", "file.bmp", "file.webm"];
+    let d = frobnicate(files);
+    println!("Generated random string: {}", d);
+}

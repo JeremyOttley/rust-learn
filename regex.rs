fc238a5d4b62b79ca96b466f3c6914b02e4cc588
Jeremy Ottley <Gazaunga@gmail.com>
2024-10-22 09:53:46 -0400

    Update regex.rs

--- regex.rs
+++ regex.rs
@@ -4,7 +4,7 @@ fn main() {
     let re = Regex::new(r"\[(?<newfilename>\w+)\]").unwrap();
     let Some(new_file_name) = re.captures("20D4EB1 [GA4D84F4].mp4") else {
         println!("no match!");
-        return;
+        Ok(())
     };
     let x = format!("{}.mp4", &new_file_name["newfilename"]);
     println!("File renamed to {}", x);
